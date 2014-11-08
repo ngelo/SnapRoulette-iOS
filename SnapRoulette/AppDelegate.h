fd85387492bda92677caaad0f795c3d1c5ba98cf
Nikolas Gelo <nikolas.gelo@gmail.com>
2014-11-08 04:06:01 -0700

    Initial Commit

---
+++ SnapRoulette/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnapRoulette
+//
+//  Created by Nikolas Gelo on 11/8/14.
+//  Copyright (c) 2014 Nikolas Gelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnapRoulette/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  SnapRoulette
+//
+//  Created by Nikolas Gelo on 11/8/14.
+//  Copyright (c) 2014 Nikolas Gelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
