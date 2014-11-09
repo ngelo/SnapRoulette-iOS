2c4a5a8c9c886c9f2f26e8d440e99cf124babf7e
Nikolas Gelo <nikolas.gelo@gmail.com>
2014-11-08 23:46:42 -0800

    Base roulette view controller created.

---
+++ SnapRoulette/RouletteViewController.h
@@ -0,0 +1,15 @@
+//
+//  RouletteViewController.h
+//  SnapRoulette
+//
+//  Created by Nikolas Gelo on 11/8/14.
+//  Copyright (c) 2014 Nikolas Gelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RouletteViewController : UIViewController
+
+
+@end
+
---
+++ SnapRoulette/RouletteViewController.m
@@ -0,0 +1,37 @@
+//
+//  RouletteViewController.m
+//  SnapRoulette
+//
+//  Created by Nikolas Gelo on 11/8/14.
+//  Copyright (c) 2014 Nikolas Gelo. All rights reserved.
+//
+
+#import "RouletteViewController.h"
+
+@interface RouletteViewController ()
+
+@property (nonatomic, weak) UIImageView *snapImageView;
+
+- (void)loadSnaps;
+
+@end
+
+@implementation RouletteViewController
+
+#pragma mark - RouletteViewController
+
+- (void)loadSnaps
+{
+    
+}
+
+#pragma mark - UIViewController
+#pragma mark Managing the View
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
